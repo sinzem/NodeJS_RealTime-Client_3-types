f3d8bd6560df0314c19147c6c9f3d8e2af1a2bd3
Sergey <sinzem@gmail.com>
2024-07-25 00:53:36 +0300

    LongPulling_bed

--- client/src/Longpulling.js
+++ client/src/Longpulling.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Longpulling = () => {
 
     const [messages, setMessages] = useState([]); /* (массив для сообщений) */
     const [value, setValue] = useState(''); /* (для данных из инпута) */
+    // const [subscrState, setSubscrState] = useState(0);
+
+    useEffect(() => { /* (при загрузке запускается функция подписки(подается запрос и висит до получения ответа или пока время не закончится, после(в любом из вариантов) автоматически перезапустится)) */
+        subscribe();
+    }, [])
+
+    const subscribe = async () => { /* (функция запроса - подписка) */
+        // let a = subscrState + 1;
+        // setSubscrState(a);
+        // console.log(subscrState);
+        try {
+            const {data} = await axios.get('http://localhost:5000/get-messages'); /* (запрос на эндпоинт) */
+            // console.log(data);
+            setMessages(prev => [data, ...prev]); /* (добавляем полученный ответ в состояния(массив сообщений)) */
+            await subscribe(); /* (перезапускаем запрос) */
+        } catch (e) {
+            setTimeout(() => {
+                subscribe(); /* (перезапускаем запрос в случае ошибки - скорее всего по истечению времени запроса) */
+            }, 500);
+        }
+    }
 
     const sendMessage = async () => { /* (функция для отправки сообщения(value из инпута) на сервер(добавляем к адресу эндпоинт), навешиваем на кнопку) */
         await axios.post('http://localhost:5000/new-messages', {
             message: value,
             id: Date.now()
-        })
+        }) ;
     }
 
     return (
--- server/longpulling.js
+++ server/longpulling.js
@@ -9,19 +9,21 @@ const emitter = new events.EventEmitter(); /* (для работы с событ
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 /* (создаем эндпоинты) */
-app.get('get-messages', (req, res) => { /* (первым пишем путь запроса, далее cb-функция) */
+app.get('/get-messages', (req, res) => { /* (первым пишем путь запроса, далее cb-функция) */
     emitter.once('newMessage', (message) => { /* (рассылаем на гет-запросы(постоянно висят в ожидании) полученное в post сообщение - получаем чат(один прислал сообщение - сервер разослал на всех подключенных)) */
+        console.log(message);
         res.json(message);
     })
 })
 
-app.post('new-messages', (req, res) => {
+app.post('/new-messages', ((req, res) => {
     const message = req.body; /* (получаем из пост-запроса сообщение) */
     emitter.emit('newMessage', message); /* (передаем на рассылку в гет-эндпоинт(по имени)) */
     res.status(200);
-})
+}))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
